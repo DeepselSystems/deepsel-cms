2114e93966ded047728da3f8c57206b341c073b7
loc <loc.truongvan263@gmail.com>
2026-03-03 18:37:50 +0700

    update theme map, and render client pages

--- client/src/themes.ts
+++ client/src/themes.ts
@@ -3,6 +3,15 @@ import Interlinked404 from '../../themes/interlinked/404.astro';
 import InterlinkedBlog from '../../themes/interlinked/blog.astro';
 import InterlinkedIndex from '../../themes/interlinked/index.astro';
 import InterlinkedSingleBlog from '../../themes/interlinked/single-blog.astro';
+import ThemeAlricos404 from '../../themes/theme_alricos/404.astro';
+import ThemeAlricosBlog from '../../themes/theme_alricos/Blog.astro';
+import ThemeAlricosIndex from '../../themes/theme_alricos/Index.astro';
+import ThemeAlricosSingleBlog from '../../themes/theme_alricos/single-blog.astro';
+import ThemeAlricosFinance from '../../themes/theme_alricos/finance.astro';
+import ThemeAlricosUnternehmensberatung from '../../themes/theme_alricos/unternehmensberatung.astro';
+import ThemeAlricosSteuerberatung from '../../themes/theme_alricos/steuerberatung.astro';
+import ThemeAlricosPersonaladministration from '../../themes/theme_alricos/personaladministration.astro';
+import ThemeAlricosKontakt from '../../themes/theme_alricos/kontakt.astro';
 // THEME_IMPORTS_END
 
 export const themeSystemKeys = {
@@ -19,6 +28,17 @@ export const themeMap = {
     [themeSystemKeys.BlogList]: InterlinkedBlog,
     [themeSystemKeys.BlogPost]: InterlinkedSingleBlog,
     [themeSystemKeys.NotFound]: Interlinked404,
+  },
+  theme_alricos: {
+    [themeSystemKeys.Page]: ThemeAlricosIndex,
+    [themeSystemKeys.BlogList]: ThemeAlricosBlog,
+    [themeSystemKeys.BlogPost]: ThemeAlricosSingleBlog,
+    [themeSystemKeys.NotFound]: ThemeAlricos404,
+    'finance': ThemeAlricosFinance,
+    'unternehmensberatung': ThemeAlricosUnternehmensberatung,
+    'steuerberatung': ThemeAlricosSteuerberatung,
+    'personaladministration': ThemeAlricosPersonaladministration,
+    'kontakt': ThemeAlricosKontakt,
   },
 };
 // THEME_MAP_END
