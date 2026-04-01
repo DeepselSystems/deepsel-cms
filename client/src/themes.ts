b7515fab71ea35415fbd9d8c92b245cf19bc2576
Tim Tran <89642525+timtran1@users.noreply.github.com>
2026-04-01 18:05:27 +0700

    clean themes

--- client/src/themes.ts
+++ client/src/themes.ts
@@ -4,16 +4,6 @@ import StarterReactBlog from '../../themes/starter_react/Blog.astro';
 import StarterReactPage from '../../themes/starter_react/page.astro';
 import StarterReactSearch from '../../themes/starter_react/search.astro';
 import StarterReactSingleBlog from '../../themes/starter_react/single-blog.astro';
-import Alcoris404 from '../../themes/alcoris/404.astro';
-import AlcorisBlog from '../../themes/alcoris/Blog.astro';
-import AlcorisIndex from '../../themes/alcoris/Index.astro';
-import AlcorisPage from '../../themes/alcoris/page.astro';
-import AlcorisSingleBlog from '../../themes/alcoris/single-blog.astro';
-import AlcorisFinance from '../../themes/alcoris/finance.astro';
-import AlcorisUnternehmensberatung from '../../themes/alcoris/unternehmensberatung.astro';
-import AlcorisSteuerberatung from '../../themes/alcoris/steuerberatung.astro';
-import AlcorisPersonaladministration from '../../themes/alcoris/personaladministration.astro';
-import AlcorisKontakt from '../../themes/alcoris/kontakt.astro';
 // THEME_IMPORTS_END
 
 export const themeSystemKeys = {
@@ -33,18 +23,6 @@ export const themeMap = {
     [themeSystemKeys.BlogPost]: StarterReactSingleBlog,
     [themeSystemKeys.SearchResults]: StarterReactSearch,
     [themeSystemKeys.NotFound]: StarterReact404,
-  },
-  alcoris: {
-    [themeSystemKeys.Home]: AlcorisIndex,
-    [themeSystemKeys.Page]: AlcorisPage,
-    [themeSystemKeys.BlogList]: AlcorisBlog,
-    [themeSystemKeys.BlogPost]: AlcorisSingleBlog,
-    [themeSystemKeys.NotFound]: Alcoris404,
-    finance: AlcorisFinance,
-    unternehmensberatung: AlcorisUnternehmensberatung,
-    steuerberatung: AlcorisSteuerberatung,
-    personaladministration: AlcorisPersonaladministration,
-    kontakt: AlcorisKontakt,
   },
 };
 // THEME_MAP_END
