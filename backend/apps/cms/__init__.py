9558a268dfaeb7ee2db225769d7f4451069d07d2
Tim Tran <89642525+timtran1@users.noreply.github.com>
2026-05-19 15:59:07 +0700

    fix ci

--- backend/apps/cms/__init__.py
+++ backend/apps/cms/__init__.py
@@ -200,7 +200,7 @@ async def set_default_domains(db):
 
 
 def set_default_theme_if_empty(db):
-    """Set default theme to alcoris if not already set, and load its seed data."""
+    """Set default theme to starter_react if not already set, and load its seed data."""
     logger.info("Checking and setting default theme if needed")
     try:
         orgs_without_theme = (
@@ -212,13 +212,13 @@ def set_default_theme_if_empty(db):
         if orgs_without_theme:
             for org in orgs_without_theme:
                 logger.info(f"Setting default theme for organization ID: {org.id}")
-                org.selected_theme = "alcoris"
+                org.selected_theme = "starter_react"
 
             db.commit()
 
             from .utils.setup_themes import load_seed_data_for_theme
 
-            load_seed_data_for_theme("alcoris", db, organization_id=1)
+            load_seed_data_for_theme("starter_react", db, organization_id=1)
             logger.info("Default theme set successfully")
     except Exception as e:
         logger.error(f"Error setting default theme: {e}")
