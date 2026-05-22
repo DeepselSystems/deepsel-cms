a535de475ef37d4e2c5f5b791814176524660904
Tim Tran <89642525+timtran1@users.noreply.github.com>
2026-05-22 11:08:12 +0700

    update

--- admin/src/common/stores/BackendHostURLState.js
+++ admin/src/common/stores/BackendHostURLState.js
@@ -1,15 +1,22 @@
 import { create } from 'zustand';
 import envBackendHost from '../../constants/backendHost.js';
 
+const normalize = (host) => {
+  if (host == null || host === '') return host;
+  if (host.endsWith('/api/v1')) return host;
+  return host.endsWith('/') ? host + 'api/v1' : host + '/api/v1';
+};
+
 const initialState = {
-  backendHost: localStorage.getItem('backendHost') || envBackendHost,
+  backendHost: normalize(localStorage.getItem('backendHost')) || envBackendHost,
 };
 
 export default create((set) => ({
   ...initialState,
   setBackendHost: (backendHost) => {
-    localStorage.setItem('backendHost', backendHost);
-    set(() => ({ backendHost }));
+    const next = normalize(backendHost);
+    localStorage.setItem('backendHost', next);
+    set(() => ({ backendHost: next }));
   },
   resetDefault: () => set(() => ({ backendHost: envBackendHost })),
 }));
