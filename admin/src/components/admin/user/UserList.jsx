ff3bde618fa9ffc4cd88a3cbe5aea2b7b1b3e9d5
pak <anhkhuong975@gmail.com>
2026-06-18 19:21:47 +0700

    eIAM (demo): User avatar has an error

--- admin/src/components/admin/user/UserList.jsx
+++ admin/src/components/admin/user/UserList.jsx
@@ -5,17 +5,17 @@ import H1 from '../../../common/ui/H1.jsx';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import { Alert } from '@mantine/core';
+import { Alert, Avatar } from '@mantine/core';
 import ListViewSearchBar from '../../../common/ui/ListViewSearchBar.jsx';
 import LinkedCell from '../../../common/ui/LinkedCell.jsx';
 import DataGridColumnMenu from '../../../common/ui/DataGridColumnMenu.jsx';
 import ListViewPagination from '../../../common/ui/ListViewPagination.jsx';
 import { Link } from 'react-router-dom';
 import Button from '../../../common/ui/Button.jsx';
-import FileDisplay from '../../../common/ui/FileDisplay.jsx';
 import Chip from '../../../common/ui/Chip.jsx';
 import OrganizationIdState from '../../../common/stores/OrganizationIdState.js';
 import { IconAlertTriangle, IconPlus } from '@tabler/icons-react';
+import { getAttachmentByNameRelativeUrl } from '@deepsel/cms-utils';
 
 export default function UserList() {
   const { t } = useTranslation();
@@ -62,7 +62,16 @@ export default function UserList() {
       width: 200,
       renderCell: (params) => (
         <LinkedCell params={params}>
-          <FileDisplay type="image" src={params.row?.image?.name} width={30} height={30} />
+          <Avatar
+            name={params.row?.image?.name || ''}
+            color="initials"
+            src={
+              params.row?.image?.name
+                ? getAttachmentByNameRelativeUrl(params.row?.image?.name)
+                : null
+            }
+            size="md"
+          />
         </LinkedCell>
       ),
     },
