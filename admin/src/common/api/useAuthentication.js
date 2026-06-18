daa6c4e9490adf9b75241fd17338cc328945c376
pak <anhkhuong975@gmail.com>
2026-06-19 00:09:08 +0700

    eIAM (demo):  cannot Publish page

--- admin/src/common/api/useAuthentication.js
+++ admin/src/common/api/useAuthentication.js
@@ -1,12 +1,21 @@
+import { useEffect } from 'react';
 import { useAuthentication as useAuthenticationBase } from '../lib/hooks';
 import BackendHostURLState from '../stores/BackendHostURLState.js';
 import UserState from '../stores/UserState.js';
 import OrganizationIdState from '../stores/OrganizationIdState.js';
+import { LocalstorageKey } from '../../constants/localstorage.js';
 
 export default function useAuthentication() {
   const { backendHost } = BackendHostURLState();
   const { user, setUser } = UserState();
   const { organizationId, setOrganizationId } = OrganizationIdState();
+
+  /**
+   * Set organizationId in localStorage when it changes
+   */
+  useEffect(() => {
+    localStorage.setItem(LocalstorageKey.OrganizationId, organizationId && String(organizationId));
+  }, [organizationId]);
 
   return useAuthenticationBase({
     backendHost,
